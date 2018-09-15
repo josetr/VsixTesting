83712a3865826de8284a1097c23044da1705f53d
josetr <37419832+josetr@users.noreply.github.com>
2018-09-15 22:03:51 +0100

    Add ability to copy VSIX to output directory

---
+++ test/VsixTesting.Xunit.Tests/ProjectReferenceTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2018 Jose Torres. All rights reserved. Licensed under the Apache License, Version 2.0. See LICENSE.md file in the project root for full license information.
+
+namespace VsixTesting.XunitX.Tests
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Xunit;
+
+    public class ProjectReferenceTests
+    {
+        const bool CopyVsixDefaultValue = true;
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(true, "CopyVsix", "True.Relative")]
+        [InlineData(true, "CopyVsix", "True.Absolute")]
+        [InlineData(false, "CopyVsix", "False")]
+        void CopyVsix(bool? copyVsix, params string[] paths)
+        {
+            var path = Path.Combine(paths.Concat(new[] { "VsixTesting.Invoker.vsix" }).ToArray());
+            Assert.Equal(copyVsix.GetValueOrDefault(CopyVsixDefaultValue), File.Exists(path));
+        }
+    }
+}
