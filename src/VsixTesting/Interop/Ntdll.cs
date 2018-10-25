986a80a0bca7abcfa7c69dd91f857087e6120cf0
josetr <37419832+josetr@users.noreply.github.com>
2018-10-25 05:17:26 +0100

    Execute tests using the same Visual Studio SKU as parent

---
+++ src/VsixTesting/Interop/Ntdll.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2018 Jose Torres. All rights reserved. Licensed under the Apache License, Version 2.0. See LICENSE.md file in the project root for full license information.
+#pragma warning disable SA1310 // Field names should not contain underscore
+
+namespace VsixTesting.Interop
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal class Ntdll
+    {
+        public const int STATUS_SUCCESS = 0;
+
+        [DllImport("ntdll.dll")]
+        public static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref PROCESS_BASIC_INFORMATION processInformation, int processInformationLength, out int returnLength);
+
+        public struct PROCESS_BASIC_INFORMATION
+        {
+            public IntPtr ExitStatus;
+            public IntPtr PebBaseAddress;
+            public IntPtr AffinityMask;
+            public IntPtr BasePriority;
+            public UIntPtr UniqueProcessId;
+            public IntPtr InheritedFromUniqueProcessId;
+            public int Size => Marshal.SizeOf(typeof(PROCESS_BASIC_INFORMATION));
+        }
+    }
+}
---
+++ src/VsixTesting/Utilities/ProcessUtil.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2018 Jose Torres. All rights reserved. Licensed under the Apache License, Version 2.0. See LICENSE.md file in the project root for full license information.
+
+namespace VsixTesting.Utilities
+{
+    using System;
+    using System.Diagnostics;
+    using VsixTesting.Interop;
+
+    internal struct ProcessUtil
+    {
+        public static Process TryGetParentProcess(Process process)
+        {
+            try
+            {
+                var processInformation = default(Ntdll.PROCESS_BASIC_INFORMATION);
+                if (Ntdll.NtQueryInformationProcess(process.Handle, 0, ref processInformation, processInformation.Size, out var returnLength) == Ntdll.STATUS_SUCCESS)
+                    return Process.GetProcessById(processInformation.InheritedFromUniqueProcessId.ToInt32());
+            }
+            catch
+            {
+            }
+
+            return null;
+        }
+
+        public static Process TryGetParentProcess(Process process, Func<Process, bool> predicate)
+        {
+            while (true)
+            {
+                process = TryGetParentProcess(process);
+                if (process == null)
+                    return null;
+
+                if (predicate(process))
+                    return process;
+            }
+        }
+    }
+}
